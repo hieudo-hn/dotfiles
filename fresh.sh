ad932ee1645aba2750465c001d2edd2dcc59cfc3
Hieu <hdo@colgate.edu>
2022-03-22 21:43:01 -0400

    First customization

--- fresh.sh
+++ fresh.sh
@@ -27,29 +27,22 @@ brew tap homebrew/bundle
 brew bundle --file $DOTFILES/Brewfile
 
 # Set default MySQL root password and auth type
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+# mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
-pecl install imagick redis swoole
+# pecl install imagick redis swoole
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose spatie/global-ray
+# /usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose spatie/global-ray
 
 # Install Laravel Valet
-$HOME/.composer/vendor/bin/valet install
+# $HOME/.composer/vendor/bin/valet install
 
 # Install Global Ray
-$HOME/.composer/vendor/bin/global-ray install
-
-# Create a Sites directory
-mkdir $HOME/Sites
-
-# Create sites subdirectories
-mkdir $HOME/Sites/blade-ui-kit
-mkdir $HOME/Sites/laravel
+# $HOME/.composer/vendor/bin/global-ray install
 
 # Clone Github repositories
-$DOTFILES/clone.sh
+# $DOTFILES/clone.sh
 
 # Symlink the Mackup config file to the home directory
 ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
